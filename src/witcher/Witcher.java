9c89ea94d56186da5787e1f7556b0455ec6f9bf5
f941033 <f941033@gmail.com>
2023-01-11 12:39:39 +0100

    añadidos atributos

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -1,6 +1,10 @@
 package witcher;
 
 public class Witcher {
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
