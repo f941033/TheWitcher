8f8268b25ae941e4014c77bcabfc9432600c736d
f941033 <f941033@gmail.com>
2023-01-11 13:40:00 +0100

    método isDeath

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -35,10 +35,27 @@ public class Witcher {
 		this.hasSoul = hasSoul;
 	}
 
+	public boolean isDeath() {
+		if(health==0) {
+			return true;
+		}else if(!hasSoul) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Witcher brujo = new Witcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(15);
+		brujo.setHasSoul(false);
+		if(brujo.isDeath()) {
+			System.out.println(brujo.getName() + " está muerto");
+		}else {
+			System.out.println(brujo.getName() + " está vivo");
+		}
 	}
 
 }
